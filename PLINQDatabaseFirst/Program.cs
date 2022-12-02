cd9630f3f53bd42c542c4f137bf311bc4d877e27
Gazi Hataş <gazihatas71@gmail.com>
2022-12-02 10:41:30 +0300

    Entity üzerinde PLINQ sorgu yazmak

--- PLINQDatabaseFirst/Program.cs
+++ PLINQDatabaseFirst/Program.cs
@@ -6,14 +6,38 @@ namespace PLINQDatabaseFirst
 {
     internal class Program
     {
+        private static void WriteLog(Product p)
+        {
+            //log'a yazdık
+            Console.WriteLine(p.Name + "log'a kaydedildi.");
+        }
+
         static void Main(string[] args)
         {
             AdventureWorks2017Context context = new AdventureWorks2017Context();
 
-            context.Products.Take(10).ToList().ForEach(x =>
-            {
-                Console.WriteLine(x.Name);
-            });
+            //context.Products.Take(10).ToList().ForEach(x =>
+            //{
+            //    Console.WriteLine(x.Name);
+            //});
+
+
+            //var product = (from p in context.Products.AsParallel()
+            //               where p.ListPrice > 10M
+            //               select p).Take(10);
+             context.Products.AsParallel().ForAll(p=>
+             {
+                 
+               WriteLog(p);  
+             });
+
+            //product.ToList().ForEach(x =>
+            //{
+            //    Console.WriteLine(x);
+            //});
+
+            //product.ForAll(x => { Console.WriteLine(x.Name); });
         }
+
     }
 }
