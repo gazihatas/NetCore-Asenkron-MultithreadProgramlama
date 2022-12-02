dd9ff7a2146e2257a3412220b361d893ac696ac7
Gazi Hataş <gazihatas71@gmail.com>
2022-12-02 08:28:02 +0300

    Parallel.For/ForEach Thread-Local Variables

---
+++ ParallelFor-ForEach-Thread-LocalVariables/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelFor_ForEach_Thread_LocalVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+
+            Parallel.ForEach(Enumerable.Range(1, 100).ToList(), () => 0, (x, loop, subtotal) =>
+            {
+                subtotal += x;
+                return subtotal;
+            }, (y)=> Interlocked.Add(ref total, y));
+
+
+            Console.WriteLine(total);
+
+            Parallel.For(0, 100, () => 0, (x, LoaderOptimization, subtotal) =>
+            {
+                subtotal = x;
+                return subtotal;
+            }, (y)=>Interlocked.Add(ref total, y));
+        }
+    }
+}
