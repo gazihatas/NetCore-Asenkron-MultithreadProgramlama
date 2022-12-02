189f8c40561f66cb21c8858232dfe46d5ff06a04
Gazi Hataş <gazihatas71@gmail.com>
2022-12-02 08:59:33 +0300

    AsParallel()

---
+++ PLINQAPP/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace PLINQAPP
+{
+    internal class Program
+    {
+        private static bool Islem(int x)
+        {
+            return x % 2 == 0;
+        }
+
+        static void Main(string[] args)
+        {
+
+            var array = Enumerable.Range(1, 100).ToList();
+            
+            array.Where(x => x % 2 == 0).ToList().ForEach(x =>
+            {
+                Console.WriteLine(x);
+            });
+
+            
+            var newArray = array.AsParallel().Where(x => x % 2 == 0);
+
+            newArray.ToList().ForEach(x =>
+            {
+                Console.WriteLine(x);
+            });
+        }
+    }
+}
