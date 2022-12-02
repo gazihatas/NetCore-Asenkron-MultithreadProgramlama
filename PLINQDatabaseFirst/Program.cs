6b96ec3ec1fc827700fd400b708fd1daa6390e38
Gazi Hataş <gazihatas71@gmail.com>
2022-12-02 10:11:12 +0300

    AdventureWorks Database First

---
+++ PLINQDatabaseFirst/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using PLINQDatabaseFirst.Models;
+
+namespace PLINQDatabaseFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AdventureWorks2017Context context = new AdventureWorks2017Context();
+
+            context.Products.Take(10).ToList().ForEach(x =>
+            {
+                Console.WriteLine(x.Name);
+            });
+        }
+    }
+}
