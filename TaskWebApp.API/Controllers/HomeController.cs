49b024fe7eafdbb7148e644422f9ca48422ff961
Gazi Hataş <gazihatas71@gmail.com>
2022-12-02 06:59:53 +0300

    CancellationToken 2

--- TaskWebApp.API/Controllers/HomeController.cs
+++ TaskWebApp.API/Controllers/HomeController.cs
@@ -7,15 +7,49 @@ namespace TaskWebApp.API.Controllers
     [ApiController]
     public class HomeController : Controller
     {
-        [HttpGet]
-        public async Task<IActionResult> GetContentAsync()
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
         {
-            Thread.Sleep(5000);
-            var mytask = new HttpClient().GetStringAsync("https://www.google.com");
+            _logger = logger;
+        }
 
-            var data = await mytask;
+        [HttpGet]
+        public async Task<IActionResult> GetContentAsync(CancellationToken token)
+        {
+            try
+            {
+                _logger.LogInformation("İstek başladı");
 
-            return Ok(data);  
+                /*
+                await Task.Delay(5000, token);
+
+                //Manuel metot ile istedği sonlandırma
+                token.ThrowIfCancellationRequested();
+
+                var mytask = new HttpClient().GetStringAsync("https://www.google.com");
+
+                var data = await mytask;
+                */
+
+                Enumerable.Range(1, 10).ToList().ForEach(x =>
+                {
+                    Thread.Sleep(1000);
+
+                    token.ThrowIfCancellationRequested();
+                    ;
+                });
+                
+                _logger.LogInformation("İstek bitti");
+
+                //return Ok(data);
+                return Ok("İşler Bitti");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation("İstek iptal edildi:" + ex.Message);
+                return BadRequest();
+            }   
         }
     }
 }
