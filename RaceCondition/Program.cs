a82a90e981dba33aceacc52d6679a950a845c379
Gazi Hataş <gazihatas71@gmail.com>
2022-12-02 08:00:37 +0300

    Race Condition Örnek

---
+++ RaceCondition/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RaceCondition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int deger = 0;
+
+            Parallel.ForEach(Enumerable.Range(1, 100000).ToList(), (x) =>
+            {
+                deger = x;
+            });
+
+            Console.WriteLine(deger);
+        }
+    }
+}
