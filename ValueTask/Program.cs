69f40f7148e0727016ddbe55335eb79c6389f210
Gazi Hataş <gazihatas71@gmail.com>
2022-12-02 07:17:14 +0300

    value task

---
+++ ValueTask/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ValueTask
+{
+    internal class Program
+    {
+        public static int cachedata { get; set; }
+        private  async static Task Main(string[] args)
+        {
+            var mytask = GetData();
+        }
+
+        public static ValueTask<int> GetData()
+        {
+            return new ValueTask<int>(cachedata);
+        }
+    }
+}
