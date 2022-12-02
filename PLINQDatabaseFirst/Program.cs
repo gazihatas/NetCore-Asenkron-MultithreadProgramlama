386b3f323b09d895b7916c9a955e46546f596921
Gazi Hataş <gazihatas71@gmail.com>
2022-12-02 11:02:25 +0300

    Exception Handle

--- PLINQDatabaseFirst/Program.cs
+++ PLINQDatabaseFirst/Program.cs
@@ -6,6 +6,19 @@ namespace PLINQDatabaseFirst
 {
     internal class Program
     {
+        private static bool IsControl(Product p)
+        {
+            try
+            {
+                return p.Name[2] == 'a';
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Dizi sınırları aşıldı");
+                return false;
+            }
+        }
+
         private static void WriteLog(Product p)
         {
             //log'a yazdık
@@ -16,6 +29,42 @@ namespace PLINQDatabaseFirst
         {
             AdventureWorks2017Context context = new AdventureWorks2017Context();
 
+
+            var products = context.Products.Take(100).ToArray();
+
+            products[3].Name = "##";
+
+            products[5].Name = "##";
+
+            var query = products.AsParallel().Where(IsControl);
+
+            try
+            {
+                query.ForAll(x =>
+                {
+                    Console.WriteLine($"{x.Name}");
+                });
+            }
+            catch (AggregateException ex)
+            {
+                ex.InnerExceptions.ToList().ForEach(x =>
+                {
+                    if (x is IndexOutOfRangeException)
+                    {
+                        Console.WriteLine($"Hata: array sınırları dışına çıkıldı");
+                    }
+                    
+                });
+                
+            }
+
+
+            //context.Products.AsParallel().AsOrdered().Where(p => p.ListPrice > 10M).ToList().ForEach(x =>
+            //{
+            //    Console.WriteLine($"{x.Name} - {x.ListPrice}");
+            //});
+
+
             //context.Products.Take(10).ToList().ForEach(x =>
             //{
             //    Console.WriteLine(x.Name);
@@ -25,11 +74,11 @@ namespace PLINQDatabaseFirst
             //var product = (from p in context.Products.AsParallel()
             //               where p.ListPrice > 10M
             //               select p).Take(10);
-             context.Products.AsParallel().WithExecutionMode(ParallelExecutionMode.ForceParallelism).ForAll(p=>
-             {
-                 
-               WriteLog(p);  
-             }); 
+            //context.Products.AsParallel().WithExecutionMode(ParallelExecutionMode.ForceParallelism).ForAll(p=>
+            //{
+
+            //  WriteLog(p);  
+            //}); 
 
             //product.ToList().ForEach(x =>
             //{
