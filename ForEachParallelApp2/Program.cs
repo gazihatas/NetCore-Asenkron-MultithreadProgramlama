20a6d3dd25f5643935fd06180500bea22d4b2c1c
Gazi Hataş <gazihatas71@gmail.com>
2022-12-02 07:56:49 +0300

    Parallel.ForEach -2

---
+++ ForEachParallelApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ForEachParallelApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long FilesByte = 0;
+
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            string picturesPath = @"C:\Users\lion\Pictures";
+
+            var files = Directory.GetFiles(picturesPath);
+
+            Parallel.ForEach(files, (item) =>
+            {
+                Console.WriteLine("thread no: " + Thread.CurrentThread.ManagedThreadId);
+
+                FileInfo f = new FileInfo(item);
+
+                Interlocked.Add(ref FilesByte, f.Length);
+
+
+            });
+
+            Console.WriteLine("toplam boyut:" + FilesByte.ToString());
+        }
+    }
+}
