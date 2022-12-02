cf132623208721945c58b1baf038df2a41d9b945
Gazi Hataş <gazihatas71@gmail.com>
2022-12-02 09:45:01 +0300

    ForAll()

---
+++ ForAll/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace ForAll
+{
+    internal class Program
+    {
+        private static bool Islem(int x)
+        {
+            return x % 2 == 0;
+        }
+        static void Main(string[] args)
+        {
+
+            var array = Enumerable.Range(1, 100).ToList();
+
+            var newArray = array.AsParallel().Where(x => x % 2 == 0);
+
+            newArray.ForAll(x =>
+            {
+                Thread.Sleep(500);
+                Console.WriteLine(x);
+            });
+        }
+    }
+}
