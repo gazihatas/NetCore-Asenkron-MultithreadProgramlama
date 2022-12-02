3f6f008ee4c2d7bcbd77501c0605bfd98226d35a
Gazi Hataş <gazihatas71@gmail.com>
2022-12-02 07:05:49 +0300

    Task Instance Result Property

---
+++ TaskInstanceResultProperty/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TaskInstanceResultProperty
+{
+    internal class Program
+    {
+        private async static Task Main(string[] args)
+        {
+            Console.WriteLine(GetData());
+        }
+
+        public static string GetData()
+        {
+            var task = new HttpClient().GetStringAsync("https://www.google.com");
+
+            return task.Result;
+        }
+    }
+}
