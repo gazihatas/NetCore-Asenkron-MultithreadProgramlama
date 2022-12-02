c13a8fa2994ef62437f3ecc2d82cfd8a4ce20d4f
Gazi Hataş <gazihatas71@gmail.com>
2022-12-02 06:10:34 +0300

    FromResult

---
+++ FromResult/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FromResult
+{
+    public class Program
+    {
+        public static string CacheData { get; set; }
+        private  async static Task Main()
+        {
+            CacheData = await GetDataAsync();
+
+            Console.WriteLine(CacheData);
+        }
+
+
+        public static Task<string> GetDataAsync()
+        {
+            if (String.IsNullOrEmpty(CacheData))
+            {
+                return File.ReadAllTextAsync("dosya.txt");
+            }
+            else
+            {
+                return Task.FromResult<string>(CacheData);
+            }
+        }
+    }
+}
